024317a5fce8101ece974f0eb035766fa017f26e
rosajc <rosajc@gmail.com>
2015-09-21 17:56:36 -0700

    Starting Xcode project
    K helped me!

---
+++ homeworkGA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homeworkGA
+//
+//  Created by Rosa Jiménez Cano on 21/9/15.
+//  Copyright © 2015 Rosa Jiménez Cano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ homeworkGATests/homeworkGATests.swift
@@ -0,0 +1,36 @@
+//
+//  homeworkGATests.swift
+//  homeworkGATests
+//
+//  Created by Rosa Jiménez Cano on 21/9/15.
+//  Copyright © 2015 Rosa Jiménez Cano. All rights reserved.
+//
+
+import XCTest
+@testable import homeworkGA
+
+class homeworkGATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
